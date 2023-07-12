a33b488256faffc3199aa51d8ea267b78f16c275
Joseph-Carter <1joseph1carter@gmail.com>
2023-07-11 21:01:08 -0400

    working with form and search.

--- main.js
+++ main.js
@@ -1,43 +1,64 @@
 const url = "https://pokeapi.co/api/v2/pokemon";
-const pokemonListPage = "https://pokeapi.co/api/v2/pokemon?limit=10"
+const pokemonListPage = "https://pokeapi.co/api/v2/pokemon?limit=10";
+const searchedPokemon = document.querySelector('.searched-pokemon')
 
 //make an api request to fetch the first 10 pokemon
-fetch(pokemonListPage)
+function pokemonListing(){
+
+    fetch(pokemonListPage)
+      .then((data) => data.json())
+      .then((data) => {
+        const pokemonContainer = document.querySelector(".pokemon-container");
+        const pokemonArray = data.results;
+    
+        pokemonArray.forEach((pokemon, index) => {
+          const pokemonId = index + 1;
+          const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+    
+          fetch(pokemonUrl)
+            .then((data) => data.json())
+            .then((pokemonData) => {
+              const pokemonName = pokemonData.name;
+              const pokemonImage = pokemonData.sprites.front_default;
+    
+              const pokemonCard = document.createElement("div");
+              pokemonCard.classList.add("pokemon-card");
+    
+              const imageElement = document.createElement("img");
+              imageElement.src = pokemonImage;
+              imageElement.alt = pokemonName;
+              imageElement.classList.add("pokemon-image");
+    
+              const pokemonNameElement = document.createElement("h2");
+              pokemonNameElement.innerText = pokemonName;
+              pokemonNameElement.classList.add("pokemon-name");
+    
+              pokemonCard.appendChild(imageElement);
+              pokemonCard.appendChild(pokemonNameElement);
+    
+              pokemonContainer.appendChild(pokemonCard);
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+}
+pokemonListing();
+
+const form = document.querySelector("#form");
+
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+
+    const id = input.value;
+    fetch(`${url}/${id}`)
     .then(data => data.json())
-    .then(data => {
-        const pokemonContainer = document.querySelector('.pokemon-container');
-        const pokemonArray = data.results
-
-        pokemonArray.forEach(pokemon => {
-            fetch(pokemon.url)
-                .then(data => data.json())
-                .then(pokemonData => {
-                    const pokemonName = pokemonData.name;
-                    const pokemonImage = pokemonData.sprites.front_default;
-
-                    const pokemonCard = document.createElement('div')
-                    pokemonCard.classList.add('pokemon-card');
-
-                    const imageElement = document.createElement('img');
-                    imageElement.src = pokemonImage;
-                    imageElement.alt = pokemonName;
-                    imageElement.classList.add('pokemon-image');
-
-                    const pokemonNameElement = document.createElement('h2');
-                    pokemonNameElement.innerText = pokemonName;
-                    pokemonNameElement.classList.add('pokemon-name');
-
-                    pokemonCard.appendChild(imageElement)
-                    pokemonCard.appendChild(pokemonNameElement);
-
-                    pokemonContainer.appendChild(pokemonCard)
-
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        }); 
+    .then(json => {
+        showpokemon(json)
     })
-    .catch(error => {
-        console.error('Error:', error)
-    })
+    .catch(error => showError(error))
+});
