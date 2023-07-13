2469792ea43b15a2dad65bf0a9b684b8e42c99b7
Joseph-Carter <1joseph1carter@gmail.com>
2023-07-12 21:03:15 -0400

    still working with functionality.

--- main.js
+++ main.js
@@ -54,20 +54,83 @@ const form = document.querySelector("#form");
 const input = document.querySelector("input");
 const pokemonInventory = document.querySelector('.searched-pokemon')
 
-searchButton.addEventListener("submit", (event) => {
-    event.preventDefault();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     const id = input.value;
     fetch(`${url}/${id}`)
-        .then(data => data.json())
-        .then(json => {
-            showPokemon(json)
-            fetch(`${url}?limit=10&offset=${json.id}`)
-                .then(data => data.json())
-                .then(data => {
-                    const pokemonContainer = document.querySelector(".pokemon-container");
-                    const pokemonArray = data.results;
+      .then((data) => data.json())
+      .then((json) => {
+        showPokemon(json);
+        fetch(`${url}?limit=10&offset=${json.id}`)
+          .then((data) => data.json())
+          .then((data) => {
+            const pokemonContainer = document.querySelector(".pokemon-container");
+  
+            // Clear existing Pokémon cards
+            pokemonContainer.innerHTML = "";
+  
+            const pokemonArray = data.results.slice(1, 11);
+  
+            pokemonArray.forEach((pokemon) => {
+              const pokemonUrl = pokemon.url;
+  
+              fetch(pokemonUrl)
+                .then((data) => data.json())
+                .then((pokemonData) => {
+                  const pokemonName = pokemonData.name;
+                  const pokemonImage = pokemonData.sprites.front_default;
+  
+                  const pokemonCard = document.createElement("div");
+                  pokemonCard.classList.add("pokemon-card");
+  
+                  const imageElement = document.createElement("img");
+                  imageElement.src = pokemonImage;
+                  imageElement.alt = pokemonName;
+                  imageElement.classList.add("pokemon-image");
+  
+                  const pokemonNameElement = document.createElement("h2");
+                  pokemonNameElement.innerText = pokemonName;
+                  pokemonNameElement.classList.add("pokemon-name");
+  
+                  pokemonCard.appendChild(imageElement);
+                  pokemonCard.appendChild(pokemonNameElement);
+  
+                  // Add click event listener to each Pokémon card
+                  pokemonCard.addEventListener("click", () => {
+                    localStorage.setItem("pokemonData", JSON.stringify(pokemonData));
+                    window.location.href = "./info.html";
+                  });
+  
+                  pokemonContainer.appendChild(pokemonCard);
                 })
-        })
-        .catch(err => showError(err))
-});
-
+                .catch((error) => {
+                  console.error("Error:", error);
+                });
+            });
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      })
+      .catch((err) => {
+        showError(err);
+      });
+  });
+  
+  const showPokemon = (json) => {
+    pokemonInventory.innerHTML = `
+      <article>
+        <img src="${json.sprites.front_default}" alt=${json.name} />
+        <h2>${json.name}</h2>
+      </article>
+    `;
+  };
+  
+  const showError = (err) => {
+    pokemonInventory.innerHTML = `
+      <section class="error">
+        <p>There was an error!</p>
+        <p class="message">${err}</p>
+      </section>
+    `;
+  };
